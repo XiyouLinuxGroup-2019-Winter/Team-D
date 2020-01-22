034b48c55f93815a36d52d3dae4a45564ec23ad5
zk <kay@xiyoulinux.org>
2020-01-22 20:41:58 +0800

    commit

--- zk/first_week_vj/L.c
+++ zk/first_week_vj/L.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+int fun(int n) {
+    int i;
+    int sum = 0;
+    for (i = 1; i <= n-1; i++) {
+        sum += i;
+    }
+    return sum;
+}
 int main()
 {
     int N;
     int time;
+    int a, b;
     while (~scanf("%d", &N)) {
-        time = 0;
-        if (N == 3) {
-            time = 1;
-        }
-        else if (N % 2 == 0) {
-            for ()
-        }
+            a = N / 2;
+            b = N - a;
+            printf("%d\n", fun(a) + fun(b));
     }
 
     return 0;
