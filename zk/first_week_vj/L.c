d9ef898a378572e84d3eef95d8b3fa0abce1e30f
zk <kay@xiyoulinux.org>
2020-01-22 19:12:07 +0800

    commit

--- zk/first_week_vj/L.c
+++ zk/first_week_vj/L.c
@@ -2,6 +2,18 @@
 
 int main()
 {
+    int N;
+    int time;
+    while (~scanf("%d", &N)) {
+        time = 0;
+        if (N == 3) {
+            time = 1;
+        }
+        else if (N % 2 == 0) {
+            for ()
+        }
+    }
+
     return 0;
 }
 
