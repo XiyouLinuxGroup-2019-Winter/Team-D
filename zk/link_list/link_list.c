a30acaa038e007c00c9e0d7ccb56e8f74f1c4289
zk <kay@xiyoulinux.org>
2020-01-23 14:28:51 +0800

    link_list

--- zk/link_list/link_list.c
+++ zk/link_list/link_list.c
@@ -28,11 +28,55 @@ struct link* initlink() {
     return head;
 }
 
+void display(struct link *p) {
+    while (p->pnext != NULL) {
+        printf("%s\n", p->name);
+        p = p->pnext;
+    }
+    printf("%s\n", p->name);
+}
+
+void top_insert(struct link *p) {
+    int position;
+    printf("please input insert position:\n");
+    scanf("%d", &position);
+    struct link *current, *insert;
+    insert = (struct link*)malloc(sizeof(struct link));
+    printf("please input insert name:\n");
+    scanf("%s", insert->name);
+    current = p;
+    int cnt = 1;
+    while (cnt != position) {
+        current = current->pnext;
+        cnt++;
+    }
+    insert->pnext = current->pnext;
+    current->pnext = insert;
+}
+
+void dellink(struct link *p) {
+    printf("please input del position:\n");
+    int position;
+    struct link *current;
+    scanf("%d", &position);
+    int cnt = 1;
+    current = p;
+    while (cnt != position) {
+        current = current->pnext;
+        cnt++;
+    }
+    current->pnext = current->pnext->pnext;
+}
+
 int main()
 {
     struct link* p;   
     p = initlink();
-
+    display(p);    
+    top_insert(p);
+    display(p);
+    dellink(p);
+    display(p);
     return 0;
 }
 
