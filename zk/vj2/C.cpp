c8b0c28d6d1557afe1d7dd7b498e882b37d636f8
zk <kay@xiyoulinux.org>
2020-02-10 12:21:01 +0800

    init

---
+++ zk/vj2/C.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int P[1000];
+int W[1000];
+int dp[10000];
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--) {
+        int E, F;
+        cin >> E >> F;
+        int weight = F -E;
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> P[i] >> W[i];
+
+        }
+    }
+    return 0;
+}
+
