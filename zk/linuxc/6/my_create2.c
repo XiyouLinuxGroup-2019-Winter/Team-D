e927cb4f90e0627edca1681c9de522b1cf056ffc
zhoukuo123 <1178053078@qq.com>
2020-03-16 12:23:57 +0800

    commit

--- zk/linuxc/6/my_create2.c
+++ zk/linuxc/6/my_create2.c
@@ -12,11 +12,14 @@ int main()
 
     if ((fd = open("test.c", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR)) == -1) {
         perror("open");
+        // printf("open:%s with errno:%d\n", strerror(errno), errno);
+        // 从errno错误代码中获取错误信息strerror(errno)
+        // 注意要包含头文件<errno.h>
         exit(1);
-    }
-    else {
+    } else {
         printf("Creat file success\n");
     }
+
     close(fd);
     return 0;
 }
